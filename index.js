efd19a0773261890bf949badb656235cfb140804
Emily Chen <emichen@mit.edu>
2025-02-28 12:16:44 -0500

    prettier error messages

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 function getRandomDog() {
-    fetch("https://dog.ceo/api/breeds/image/random")
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data.message);
-            document.querySelector("#dog-image").src = data.message;
-        });
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.message);
+      document.querySelector("#dog-image").src = data.message;
+    });
 }
 //https://dog.ceo/api/breed/hound/images/random Fetch!
 
 function getSpecificDog() {
-    let breed = document.querySelector("#breed").value;
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
-        .then((res) => {
-            if (!res.ok) {
-                alert(
-                    `Woof woof, we couldn't find that dog :( Please try again!`
-                );
-                return;
-            }
-            return res.json();
-        })
-        .then((data) => {
-            console.log(data.message);
-            document.querySelector("#dog-image").src = data.message;
-        });
+  let breed = document.querySelector("#breed").value;
+  let errorMessage = document.querySelector("#error-message");
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then((res) => {
+      if (!res.ok) {
+        errorMessage.textContent = `Woof woof, we couldn't find that dog :( Please try again!`;
+        document.querySelector("#error-message").style.visibility = "visible";
+        return;
+      }
+      document.querySelector("#error-message").style.visibility = "hidden";
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.message);
+      document.querySelector("#dog-image").src = data.message;
+    });
 }
